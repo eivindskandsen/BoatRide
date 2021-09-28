f82b5129d82573d6562dc0fff2abeca804a33bcd
MartinDAa <martindaa@hotmail.no>
2021-09-28 14:14:11 +0200

    Start inputvalidering server + logs

--- BoatRide/Controllers/BillettController.cs
+++ BoatRide/Controllers/BillettController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BoatRide.Models;
+using Microsoft.Extensions.Logging;
 
 namespace BoatRide.Controllers
 {
@@ -11,10 +12,12 @@ namespace BoatRide.Controllers
     public class BillettController : ControllerBase
     {
         private readonly BoatContext _db;
+        private ILogger<BillettController> _log;
 
-        public BillettController(BoatContext db)
+        public BillettController(BoatContext db, ILogger<BillettController> log)
         {
             _db = db;
+            _log = log;
         }
 
         public bool LagreBillett(Billett billett)
