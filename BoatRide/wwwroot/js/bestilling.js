ff4873143c1d81110e86f30837f6825a35368660
MartinDAa <martindaa@hotmail.no>
2021-09-28 11:34:18 +0200

    Bestilling.js begynnelse

--- BoatRide/wwwroot/js/bestilling.js
+++ BoatRide/wwwroot/js/bestilling.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(function () {
+    $.post("Kunde/HentEn", function (billetten) {
+        $("#navn").html(billetten.navn)
+        $("#epost").html(billetten.mail)
+        $("#ordre").html(billetten.order)
+        $("#fra").html(billetten.from)
+        $("#til").html(billetten.to)
+        $("#dato").html(billetten.date)
+        $("#pris").html(billetten.price)
+    });
+});
