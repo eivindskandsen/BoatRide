4cb4da8c7ba04240d8031f298281822c484a7dd9
JohnFredrikDall <55983529+JohnFredrikDall@users.noreply.github.com>
2021-10-06 12:07:05 +0200

    asd

--- BoatRide/Controllers/BillettController.cs
+++ BoatRide/Controllers/BillettController.cs
@@ -24,7 +24,7 @@ namespace BoatRide.Controllers
 
         }
 
-        public async Task<bool> LagreBillett(Billett billett, Kunde innKunde)
+        public async Task<bool> LagreBillett([FromBody]Billett billett, [FromBody]Kunde innKunde)
         {
             if (ModelState.IsValid)
             {
